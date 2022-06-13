3e22b09deb057c10ecaed893411db60b679862b8
nik-lazer <nick.lazarev@gmail.com>
2022-06-13 23:56:47 +0300

    remove deprecation

--- src/main/java/lan/test/oauth/oauthdemo/OauthDemoApplication.java
+++ src/main/java/lan/test/oauth/oauthdemo/OauthDemoApplication.java
@@ -2,11 +2,12 @@ package lan.test.oauth.oauthdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,11 +19,10 @@ import java.util.Optional;
 
 @SpringBootApplication
 @RestController
-public class OauthDemoApplication extends WebSecurityConfigurerAdapter {
+public class OauthDemoApplication {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // @formatter:off
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(a -> a
                         .antMatchers("/", "/error", "/webjars/**").permitAll()
@@ -43,6 +43,8 @@ public class OauthDemoApplication extends WebSecurityConfigurerAdapter {
                 )
                 .oauth2Login();
         // @formatter:on
+
+        return http.build();
     }
 
     public static void main(String[] args) {
