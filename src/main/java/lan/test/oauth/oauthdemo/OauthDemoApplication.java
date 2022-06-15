acea0ae7386c6b038603849895292b77abbf45d7
nik-lazer <nick.lazarev@gmail.com>
2022-06-16 00:18:56 +0300

    add Microsoft oAuth support

--- src/main/java/lan/test/oauth/oauthdemo/OauthDemoApplication.java
+++ src/main/java/lan/test/oauth/oauthdemo/OauthDemoApplication.java
@@ -9,10 +9,13 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
@@ -23,9 +26,11 @@ public class OauthDemoApplication {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        SimpleUrlAuthenticationFailureHandler handler = new SimpleUrlAuthenticationFailureHandler("/");
+
         http
                 .authorizeRequests(a -> a
-                        .antMatchers("/", "/error", "/webjars/**").permitAll()
+                        .antMatchers("/health", "/", "/error", "/webjars/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .logout(l -> l
@@ -41,7 +46,14 @@ public class OauthDemoApplication {
                 .csrf(c -> c
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 )
-                .oauth2Login();
+                .oauth2Login(o -> o
+                        .failureHandler((request, response, exception) -> {
+                            System.out.println("EXCEPTION="+exception);
+                            exception.printStackTrace();
+                            request.getSession().setAttribute("error.message", exception.getMessage());
+                            handler.onAuthenticationFailure(request, response, exception);
+                        })
+                );
         // @formatter:on
 
         return http.build();
@@ -56,5 +68,13 @@ public class OauthDemoApplication {
         var name = Optional.ofNullable(principal.getAttribute("name"))
                 .orElse(principal.getAttribute("login"));
         return Collections.singletonMap("name", name);
+    }
+
+    @GetMapping("/error")
+    @ResponseBody
+    public String error(HttpServletRequest request) {
+        String message = (String) request.getSession().getAttribute("error.message");
+        request.getSession().removeAttribute("error.message");
+        return message;
     }
 }
